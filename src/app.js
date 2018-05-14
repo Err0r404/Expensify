59eab5ef3630ed048e521574f4177395960a516e
Julien <schmittounet@gmail.com>
2018-05-14 22:04:29 +0200

    77

--- src/app.js
+++ src/app.js
@@ -3,7 +3,51 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Boilerplate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => {
+    return (
+        <div>
+            Dashboard's component
+        </div>
+    )
+};
+
+const AddExpensePage = () => {
+    return (
+        <div>
+            Add expense's component
+        </div>
+    )
+};
+
+const EditExpensePage = () => {
+    return (
+        <div>
+            Edit expense's component
+        </div>
+    )
+};
+
+const HelpExpensePage = () => {
+    return (
+        <div>
+            Help expense's component
+        </div>
+    )
+};
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+            <Route path="/create" component={AddExpensePage}/>
+            <Route path="/edit" component={EditExpensePage}/>
+            <Route path="/help" component={HelpExpensePage}/>
+        </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
