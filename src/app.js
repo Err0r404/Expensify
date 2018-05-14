4a22694b67b65ae2d454cd72aea954055e2f4c37
Julien <schmittounet@gmail.com>
2018-05-14 21:44:47 +0200

    76

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// npm run serve
+// npm run build
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/styles.scss';
+
+ReactDOM.render(<p>Boilerplate</p>, document.getElementById('app'));
