b568ab526cb2f9eaedc8c4dc992af2ef21b1e60f
Julien Schmitt <julien.schmitt@imaios.com>
2018-05-15 13:29:53 +0200

    79

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 
 import './styles/styles.scss';
 
@@ -42,20 +42,37 @@ const HelpPage = () => {
 const NotFoundPage = () => {
     return (
         <div>
-            Not found's component
+            <p>Not found's component</p>
+            <Link to="/">Home</Link>
         </div>
+    )
+};
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Expensify</h1>
+
+            <NavLink to="/" activeClassName="active" exact={true}>Home</NavLink> |
+            <NavLink to="/create" activeClassName="active">Add</NavLink> |
+            <NavLink to="/edit" activeClassName="active">Edit</NavLink> |
+            <NavLink to="/help" activeClassName="active">Help</NavLink>
+        </header>
     )
 };
 
 const routes = (
     <BrowserRouter>
-        <Switch>
-            <Route path="/" component={ExpenseDashboardPage} exact={true} />
-            <Route path="/create" component={AddExpensePage} />
-            <Route path="/edit" component={EditExpensePage} />
-            <Route path="/help" component={HelpPage} />
-            <Route component={NotFoundPage} />
-        </Switch>
+        <div>
+            <Header></Header>
+            <Switch>
+                <Route path="/" component={ExpenseDashboardPage} exact={true} />
+                <Route path="/create" component={AddExpensePage} />
+                <Route path="/edit" component={EditExpensePage} />
+                <Route path="/help" component={HelpPage} />
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
     </BrowserRouter>
 );
 
