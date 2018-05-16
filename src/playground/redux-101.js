7880cd6c9c2e66e2025d8acfc50bcd9d79dc3fab
Julien Schmitt <julien.schmitt@imaios.com>
2018-05-16 15:23:13 +0200

    87

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,16 +3,23 @@ import {createStore} from 'redux';
 const store = createStore((state = {count: 0}, action) => {
     switch (action.type){
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             };
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
             };
         case 'RESET':
             return {
                 count: 0
+            };
+        case 'SET':
+            const count = typeof action.count === 'number' ? action.count : 0;
+            return {
+                count: count
             };
         default:
             return state;
@@ -20,34 +27,35 @@ const store = createStore((state = {count: 0}, action) => {
     }
 });
 
-console.log(store.getState());
+store.subscribe(() => {
+    console.log(store.getState());
+});
 
 // Increment
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    incrementBy: 5
 });
 store.dispatch({
     type: 'INCREMENT'
 });
-
-console.log(store.getState());
 
 // Decrement
 store.dispatch({
     type: 'DECREMENT'
 });
 store.dispatch({
-    type: 'DECREMENT'
+    type: 'DECREMENT',
+    decrementBy: 10
 });
-store.dispatch({
-    type: 'DECREMENT'
-});
-
-console.log(store.getState());
 
 // Reset
 store.dispatch({
     type: 'RESET'
 });
 
-console.log(store.getState());
+// Set
+store.dispatch({
+    type: 'SET',
+    count: 101
+});
