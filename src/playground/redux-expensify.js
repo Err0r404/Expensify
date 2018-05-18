856df95ada73ce5468dd9a4f90fadf76d3551142
Julien Schmitt <julien.schmitt@imaios.com>
2018-05-18 10:54:59 +0200

    92

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,4 +1,36 @@
-import { createStore, combineReducers } from 'redux';
+import {createStore, combineReducers} from 'redux';
+
+// Expenses Reducer
+const expenseReducerDefaultState = [];
+const expenseReducer = (state = expenseReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+// Filters Reducer
+const filterReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined,
+};
+const filterReducer = (state = filterReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+const store = createStore(
+    combineReducers({
+        expenses: expenseReducer,
+        filters : filterReducer
+    })
+);
+
+console.log(store.getState());
 
 const demoState = {
     expenses: [
