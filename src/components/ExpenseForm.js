3420135d916d1f7a0cd38a291fa2264fe702affe
Julien Schmitt <julien.schmitt@imaios.com>
2018-05-30 13:51:53 +0200

    106

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import moment from 'moment';
+import {SingleDatePicker} from 'react-dates'
+import 'react-dates/lib/css/_datepicker.css';
 
-class ExpenseForm extends React.Component{
+// Used to change language and format in react-dates
+// moment.locale('fr');
+
+class ExpenseForm extends React.Component {
     state = {
         description: '',
         note: '',
-        amount: 0
+        amount: 0,
+        createdAt: moment(),
+        calendarFocused: false
     };
 
     onDescriptionChange = (e) => {
@@ -12,21 +20,30 @@ class ExpenseForm extends React.Component{
         this.setState(() => ({description}));
     };
 
+    onAmountChange = (e) => {
+        const amount = e.target.value;
+
+        if (amount.match(/^\d*([\.\,]\d{0,2})?$/)) {
+            this.setState(() => ({amount}));
+        }
+    };
+
+    onDateChange = (createdAt) => {
+        this.setState(() => ({createdAt}));
+    };
+
+    // See react-dates documentation
+    onCalendarFocusChange = ({focused}) => {
+        this.setState(() => ({calendarFocused: focused}));
+    };
+
     onNoteChange = (e) => {
         const note = e.target.value;
         this.setState(() => ({note}));
     };
 
-    onAmountChange = (e) => {
-        const amount = e.target.value;
-
-        if(amount.match(/^\d*([\.\,]\d{0,2})?$/)){
-            this.setState(() => ({amount}));
-        }
-    };
-
     render() {
-        return(
+        return (
             <div>
                 <form action="">
                     <input
@@ -42,6 +59,15 @@ class ExpenseForm extends React.Component{
                         placeholder="Amount"
                         value={this.state.amount}
                         onChange={this.onAmountChange}
+                    />
+
+                    <SingleDatePicker
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calendarFocused}
+                        onFocusChange={this.onCalendarFocusChange}
+                        numberOfMonths={1}
+                        isOutsideRange={(day) => {false}}
                     />
                     <br/>
 
