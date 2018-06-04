ed79985755f334e9ac7777412f674b45966db724
Julien Schmitt <julien.schmitt@imaios.com>
2018-06-04 12:44:33 +0200

    109

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import moment from 'moment';
 
 // The connect function passes in dispatch as a prop to our component when we wrap it.
 const ExpenseListItem = ({id, description, amount, createdAt}) => {
     return (
         <div>
             <h3>{description}</h3>
-            <p>{amount} - {createdAt}</p>
+            <p>{amount} - {moment(createdAt).format('DD/MM/YYYY')}</p>
 
             <Link to={`/edit/${id}`}>
                 Edit
